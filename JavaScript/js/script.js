e82d8bfa90c9a02f73a948961579038562674f4f
Yolee <vrulevich@gmail.com>
2020-08-28 12:05:51 +0300

    first commit

---
+++ JavaScript/js/script.js
@@ -0,0 +1,54 @@
+"use sctrict"; 
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+//  alert(5);
+//  [].push('a');
+
+// const obj = {
+//     name: "Jhon",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[2]);
+
+//alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Hello, ${user}`);
